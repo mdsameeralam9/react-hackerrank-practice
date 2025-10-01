be886a5d34cdda53327f93bb6fda915c1a9d5d7b
mdsameeralam9 <mdsameer.alam9@gmail.com>
2025-10-01 20:03:15 +0530

    day 3

--- Js practice files/easy.js
+++ Js practice files/easy.js
@@ -33,7 +33,8 @@ function isAlphabeticPalindrome(code) {
 console.log(isAlphabeticPalindrome("A1b2B!a"));
 
 // 3. firstMissingPositive_sort
-function firstMissingPositive_sort(nums) {
+// Simple: slower due to sort
+function firstMissingPositive_sort1(nums) {
   nums.sort((a, b) => a - b); // [-1, 1, 3, 4]
   console.log(nums)
   let need = 1;
@@ -44,6 +45,59 @@ function firstMissingPositive_sort(nums) {
   }
   return need;
 }
+
+// Simple: extra space allowed
+function firstMissingPositive_set2(nums) {
+  const s = new Set();
+  for (const v of nums) if (v > 0) s.add(v);
+ console.log(s)
+  let x = 1;
+  while (true) {
+    if (!s.has(x)) return x;
+    x++;
+  }
+}
+
+
+function firstMissingPositive_set3(arr){
+  // remove deblicate and 
+  arr = [...new Set(arr)];
+  console.log(1, arr)
+  // remove negative and zero value
+ arr = arr.filter(v => v > 0);
+  console.log(2, arr)
+ // sort
+ arr = arr.sort((a,b) => a-b);
+  console.log(3, arr)
+
+  // check for positvie
+  let need = 1;
+  for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    if(need === element) need++;
+    else return need;
+  }
+
+  return need;
+}
+
+
+function firstMissingPositive_set4(arr){
+  // check for positvie
+  let need = {};
+  for (let index = 0; index < arr.length; index++) {
+    need[arr[index]] = 1
+  }
+
+  for (let index = 1; index < arr.length; index++) {
+    if(!need[index]) return index;
+  }
+
+  return arr.length+1;
+}
+
+
+console.log(firstMissingPositive_set4([3,3,1,2,3, 4, -1, 12]));
 
 
 console.log(firstMissingPositive_sort([3, 4, -1, 1]));
