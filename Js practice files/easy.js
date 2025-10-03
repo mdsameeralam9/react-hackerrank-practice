5180c6cf189af00be50c88b1806f17959b34f084
mdsameeralam9 <mdsameer.alam9@gmail.com>
2025-10-03 09:26:15 +0530

    day 5

--- Js practice files/easy.js
+++ Js practice files/easy.js
@@ -1,7 +1,7 @@
 // 1. countResponseTimeRegressions
 function countResponseTimeRegressions(responseTimes) {
-    // Write your code here
-   const n = responseTimes.length;
+  // Write your code here
+  const n = responseTimes.length;
   if (n <= 1) return 0;
 
   let count = 0;
@@ -13,24 +13,23 @@ function countResponseTimeRegressions(responseTimes) {
     sumPrev += responseTimes[i];
   }
   return count;
-
 }
-console.log(countResponseTimeRegressions([100, 200, 150,300]));
+console.log(countResponseTimeRegressions([100, 200, 150, 300]));
 
 //-------------------------------------Day 1 End---------------------------------------
 
 // 2. isAlphabeticPalindrome
 function isAlphabeticPalindrome(code) {
-    // Write your code here
-    code = code.toLowerCase();
-    let filterStr = "";
-    for(const char of code){
-        if(char >= "a" && char <= "z"){
-            filterStr += char
-        }
+  // Write your code here
+  code = code.toLowerCase();
+  let filterStr = "";
+  for (const char of code) {
+    if (char >= "a" && char <= "z") {
+      filterStr += char;
     }
-    
-    return filterStr === filterStr.split("").reverse().join("")
+  }
+
+  return filterStr === filterStr.split("").reverse().join("");
 }
 console.log(isAlphabeticPalindrome("A1b2B!a"));
 
@@ -39,7 +38,7 @@ console.log(isAlphabeticPalindrome("A1b2B!a"));
 // Simple: slower due to sort
 function firstMissingPositive_sort1(nums) {
   nums.sort((a, b) => a - b); // [-1, 1, 3, 4]
-  console.log(nums)
+  console.log(nums);
   let need = 1;
   for (const v of nums) {
     if (v < need) continue; // It skips the current loop
@@ -53,7 +52,7 @@ function firstMissingPositive_sort1(nums) {
 function firstMissingPositive_set2(nums) {
   const s = new Set();
   for (const v of nums) if (v > 0) s.add(v);
- console.log(s)
+  console.log(s);
   let x = 1;
   while (true) {
     if (!s.has(x)) return x;
@@ -61,45 +60,43 @@ function firstMissingPositive_set2(nums) {
   }
 }
 
-
-function firstMissingPositive_set3(arr){
-  // remove deblicate and 
+function firstMissingPositive_set3(arr) {
+  // remove deblicate and
   arr = [...new Set(arr)];
-  console.log(1, arr)
+  console.log(1, arr);
   // remove negative and zero value
- arr = arr.filter(v => v > 0);
-  console.log(2, arr)
- // sort
- arr = arr.sort((a,b) => a-b);
-  console.log(3, arr)
+  arr = arr.filter((v) => v > 0);
+  console.log(2, arr);
+  // sort
+  arr = arr.sort((a, b) => a - b);
+  console.log(3, arr);
 
   // check for positvie
   let need = 1;
   for (let index = 0; index < arr.length; index++) {
     const element = arr[index];
-    if(need === element) need++;
+    if (need === element) need++;
     else return need;
   }
 
   return need;
 }
 
-
-function firstMissingPositive_set4(arr){
+function firstMissingPositive_set4(arr) {
   // check for positvie
   let need = {};
   for (let index = 0; index < arr.length; index++) {
-    need[arr[index]] = 1
+    need[arr[index]] = 1;
   }
 
   for (let index = 1; index < arr.length; index++) {
-    if(!need[index]) return index;
+    if (!need[index]) return index;
   }
 
-  return arr.length+1;
+  return arr.length + 1;
 }
 
-console.log(firstMissingPositive_set4([3,3,1,2,3, 4, -1, 12]));
+console.log(firstMissingPositive_set4([3, 3, 1, 2, 3, 4, -1, 12]));
 //-------------------------------------Day 3 End---------------------------------------
 
 // 4. isNonTrivialRotation
@@ -110,20 +107,20 @@ function isNonTrivialRotation1(s, t) {
 }
 
 function isNonTrivialRotation2(s, t) {
-  if (s.length !== t.length) return false; 
+  if (s.length !== t.length) return false;
   const n = s.length;
   for (let k = 0; k < n; k++) {
-    const rot = s.slice(k) + s.slice(0, k); 
+    const rot = s.slice(k) + s.slice(0, k);
     if (rot === t) return true;
   }
   return false;
 }
 
 function isNonIdenticalRotation3(s1, s2) {
-  if (s1.length !== s2.length) return 0;         
-  if (s1 === s2) return 0;                     
-  const doubled = s1 + s1;                       
-  return doubled.includes(s2) ? 1 : 0;     
+  if (s1.length !== s2.length) return 0;
+  if (s1 === s2) return 0;
+  const doubled = s1 + s1;
+  return doubled.includes(s2) ? 1 : 0;
 }
 // console.log(1, isNonIdenticalRotation3("abcd", "cdab")); // true
 // console.log(2, isNonIdenticalRotation3("abcde", "cdeab")); // true
@@ -151,8 +148,31 @@ function binarySearch(nums, target) {
 
   return -1;
 }
-const nums = [2, 4, 6, 8, 10, 12, 14, 16] //[1, 2, 3, 4, 5]
-const target = 16 //3 => 2
-console.log(binarySearch(nums, target))
+const nums = [2, 4, 6, 8, 10, 12, 14, 16]; //[1, 2, 3, 4, 5]
+const target = 16; //3 => 2
+console.log(binarySearch(nums, target));
+
+// findFirstOccurrence of target, array can have repeated element
+// so if found in mid the move right to left to get the first index or previous index of mid
+function findFirstOccurrence(nums, target) {
+  // Write your code here
+
+  let left = 0,
+    right = nums.length - 1,
+    ans = -1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      ans = mid;
+      right = mid - 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return ans;
+}
 
 //-------------------------------------Day 5 End---------------------------------------
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-function binarySearch(nums, target) {
+function findFirstOccurrence(nums, target) {
   // Write your code here
+
   let left = 0,
-    right = nums.length - 1;
+    right = nums.length - 1,
+    ans = -1;
   while (left <= right) {
     const mid = Math.floor((left + right) / 2);
     if (nums[mid] === target) {
-      return mid;
+      ans = mid;
+      right = mid - 1;
     } else if (nums[mid] > target) {
       right = mid - 1;
     } else {
@@ -13,8 +16,5 @@ function binarySearch(nums, target) {
     }
   }
 
-  return -1;
+  return ans;
 }
-const nums = [2, 4, 6, 8, 10, 12, 14, 16] //[1, 2, 3, 4, 5]
-const target = 16 //3 => 2
-console.log(binarySearch(nums, target))
