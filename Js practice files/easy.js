34e3856d0b2977b31cbbc671163bee06d908863c
mdsameeralam9 <mdsameer.alam9@gmail.com>
2025-10-03 07:20:57 +0530

    day 5

--- Js practice files/easy.js
+++ Js practice files/easy.js
@@ -132,3 +132,27 @@ function isNonIdenticalRotation3(s1, s2) {
 //console.log(5, isNonIdenticalRotation3("geeks for geeks", "for geeks geeks")); //false
 
 //-------------------------------------Day 4 End---------------------------------------
+
+// 5. Lookup with Binary Search
+function binarySearch(nums, target) {
+  // Write your code here
+  let left = 0,
+    right = nums.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return -1;
+}
+const nums = [2, 4, 6, 8, 10, 12, 14, 16] //[1, 2, 3, 4, 5]
+const target = 16 //3 => 2
+console.log(binarySearch(nums, target))
+
+//-------------------------------------Day 5 End---------------------------------------
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function binarySearch(nums, target) {
+  // Write your code here
+  let left = 0,
+    right = nums.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
 
+  return -1;
+}
+const nums = [2, 4, 6, 8, 10, 12, 14, 16] //[1, 2, 3, 4, 5]
+const target = 16 //3 => 2
+console.log(binarySearch(nums, target))
